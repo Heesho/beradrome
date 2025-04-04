1c4f1f73c01fdac8db0c65890dfb4a118bfc11f1
Heesho <hisham11027@gmail.com>
2025-04-04 12:18:08 -0700

    data for infrared ad

---
+++ scripts/infraredAirdrop.js
@@ -0,0 +1,119 @@
+// Name: Infrared Trifecta Kodiak Island OHM-HONEY-0.3%
+// Contract: 0x125bd220ac62DBE312D4752FFB57551E9b3c1848
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x125bd220ac62DBE312D4752FFB57551E9b3c1848
+
+// Name: Infrared BeraSwap 50WBERA-50HONEY-WEIGHTED
+// Contract: 0xF7f7cFc9FEB0f40a158D607F7Aa04B9D18aC8CA6
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xF7f7cFc9FEB0f40a158D607F7Aa04B9D18aC8CA6
+
+// Name: Infrared BeraSwap 50WETH-50WBERA-WEIGHTED
+// Contract: 0xdB79d6a9D43c69BEd341ee93d796f4aDDEF174cc
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xdB79d6a9D43c69BEd341ee93d796f4aDDEF174cc
+
+// Name: Infrared BeraSwap 50WBTC-50WBERA-WEIGHTED
+// Contract: 0x8bBFCfF7FeA31aA4d330663111b85924dBad11B8
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x8bBFCfF7FeA31aA4d330663111b85924dBad11B8
+
+// Name: Infrared BeraSwap USDCe-HONEY-STABLE
+// Contract: 0x98DBeA9494f4630773D3Ef834c0734D28C39E440
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x98DBeA9494f4630773D3Ef834c0734D28C39E440
+
+// Name: Infrared BeraSwap BYUSD-HONEY-STABLE
+// Contract: 0xdbF1a45Cf806346a26B54c447fb4840b68828cDd
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xdbF1a45Cf806346a26B54c447fb4840b68828cDd
+
+// Name: Infrared iBGT
+// Contract: 0x0AB1f3949cebB19FDebC5934d1822EA9ca24aE6F
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x0AB1f3949cebB19FDebC5934d1822EA9ca24aE6F
+
+// Name: Infrared Kodiak brWETH-HONEY-V2
+// Contract: 0x8C3B6d5dF8fEB0D5E7c995FDBD9fB08ca29F38Af
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x8C3B6d5dF8fEB0D5E7c995FDBD9fB08ca29F38Af
+
+// Name: Infrared Arbera brWBTC-WBERA-V2
+// Contract: 0xeDDC88234A4e3E940c360cF84797BF0A02300cc5
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xeDDC88234A4e3E940c360cF84797BF0A02300cc5
+
+// Name: Infrared Arbera brWETH-WBERA-V2
+// Contract: 0xd0Ed2367e61CdE493830aDdE4db4A45753a5b1dC
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xd0Ed2367e61CdE493830aDdE4db4A45753a5b1dC
+
+// Name: Infrared Arbera briBGT-HONEY-V2
+// Contract: 0xDF1F4b4DF8615A0F677DEFE74Bc172174536ef52
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xDF1F4b4DF8615A0F677DEFE74Bc172174536ef52
+
+// Name: Infrared Arbera brHENLO-HONEY-V2
+// Contract: 0xa16f765C06A94bB68FcDE4334e0aF46a96107547
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xa16f765C06A94bB68FcDE4334e0aF46a96107547
+
+// Name: Infrared Arbera briBERA-HONEY-V2
+// Contract: 0x29FA6c7b23fE69162e7A5D3288190bB67d1A3Cb8
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x29FA6c7b23fE69162e7A5D3288190bB67d1A3Cb8
+
+// Name: Infrared Arbera brBREAD-HONEY-V2
+// Contract: 0xbDDBEAbFCcA0a62b49aDDDb180D0CC4dea36f6fC
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xbDDBEAbFCcA0a62b49aDDDb180D0CC4dea36f6fC
+
+// Name: Infrared Arbera br3BC-HONEY-V2
+// Contract: 0xbB5ccd574AFe680a52f70dB8e6986E3Acc6E8185
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xbB5ccd574AFe680a52f70dB8e6986E3Acc6E8185
+
+// Name: Infrared Arbera brNAV-WBERA-V2
+// Contract: 0xc44Af1e4c4339EC7d100eDA0aF28D9815057b68b
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xc44Af1e4c4339EC7d100eDA0aF28D9815057b68b
+
+// Name: Infrared Arbera brRAMEN-HONEY-V2
+// Contract: 0xefd9d044576962C57feb950B7f14890C24f6B18c
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xefd9d044576962C57feb950B7f14890C24f6B18c
+
+// Name: Infrared Arbera brwgBERA-WBERA-V2
+// Contract: 0xD17Ea394815d98B6760b177A05239e64296f33DB
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0xD17Ea394815d98B6760b177A05239e64296f33DB
+
+// Name: Infrared Arbera brberaETH-WBERA-V2
+// Contract: 0x42e4c96C88e8FB9d57fEf775AdADECd7Ba434c2A
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x42e4c96C88e8FB9d57fEf775AdADECd7Ba434c2A
+
+// Name: Infrared Arbera brYEET-HONEY-V2
+// Contract: 0x2D9AEDee285D6e3eDD082162ca83ab28E4382891
+// Option: Direct Airdrop to Users
+// Description: Staking pool, users stake in contract, contract stakes in Infrared Vault
+// Interface: https://www.beradrome.com/farms/0x2D9AEDee285D6e3eDD082162ca83ab28E4382891
