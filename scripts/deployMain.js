f0ba8ff84c762c6ec39d91431c82a9920724f1a9
Heesho <hisham11027@gmail.com>
2025-05-21 16:59:22 -0700

    ran distribute

--- scripts/deployMain.js
+++ scripts/deployMain.js
@@ -2191,54 +2191,54 @@ async function main() {
   // 13. Distro
   //===================================================================
 
-  // console.log("Distributing Rewards");
+  console.log("Distributing Rewards");
 
-  // await voter.distro();
-  // console.log("Gauge Rewards Distributed");
+  await voter.distro();
+  console.log("Gauge Rewards Distributed");
 
-  // await fees.distribute();
-  // console.log("Fees Rewards Distributed");
+  await fees.distribute();
+  console.log("Fees Rewards Distributed");
 
-  // await voter.distributeToBribes([
-  //   BERACHAIN_PLUGIN_0,
-  //   BERACHAIN_PLUGIN_1,
-  //   BERACHAIN_PLUGIN_2,
-  //   BERACHAIN_PLUGIN_3,
-  //   BERACHAIN_PLUGIN_4,
-  // ]);
-  // console.log("Berachain Bribe Rewards Distributed");
+  await voter.distributeToBribes([
+    BERACHAIN_PLUGIN_0,
+    BERACHAIN_PLUGIN_1,
+    BERACHAIN_PLUGIN_2,
+    BERACHAIN_PLUGIN_3,
+    BERACHAIN_PLUGIN_4,
+  ]);
+  console.log("Berachain Bribe Rewards Distributed");
 
-  // await voter.distributeToBribes([
-  //   INFRARED_PLUGIN_0,
-  //   INFRARED_PLUGIN_1,
-  //   INFRARED_PLUGIN_2,
-  //   INFRARED_PLUGIN_3,
-  //   INFRARED_PLUGIN_4,
-  //   INFRARED_PLUGIN_5,
-  //   INFRARED_PLUGIN_6,
-  //   INFRARED_PLUGIN_7,
-  //   INFRARED_PLUGIN_8,
-  //   INFRARED_PLUGIN_9,
-  //   INFRARED_PLUGIN_10,
-  //   INFRARED_PLUGIN_11,
-  //   INFRARED_PLUGIN_12,
-  //   INFRARED_PLUGIN_13,
-  //   INFRARED_PLUGIN_14,
-  //   INFRARED_PLUGIN_15,
-  //   INFRARED_PLUGIN_16,
-  //   INFRARED_PLUGIN_17,
-  //   INFRARED_PLUGIN_18,
-  // ]);
-  // console.log("Infrared Bribe Rewards Distributed");
+  await voter.distributeToBribes([
+    INFRARED_PLUGIN_0,
+    INFRARED_PLUGIN_1,
+    INFRARED_PLUGIN_2,
+    INFRARED_PLUGIN_3,
+    INFRARED_PLUGIN_4,
+    INFRARED_PLUGIN_5,
+    INFRARED_PLUGIN_6,
+    INFRARED_PLUGIN_7,
+    INFRARED_PLUGIN_8,
+    INFRARED_PLUGIN_9,
+    INFRARED_PLUGIN_10,
+    INFRARED_PLUGIN_11,
+    INFRARED_PLUGIN_12,
+    INFRARED_PLUGIN_13,
+    INFRARED_PLUGIN_14,
+    INFRARED_PLUGIN_15,
+    INFRARED_PLUGIN_16,
+    INFRARED_PLUGIN_17,
+    INFRARED_PLUGIN_18,
+  ]);
+  console.log("Infrared Bribe Rewards Distributed");
 
-  // await voter.distributeToBribes([
-  //   BERAPAW_PLUGIN_0,
-  //   BERAPAW_PLUGIN_1,
-  //   BERAPAW_PLUGIN_2,
-  //   BERAPAW_PLUGIN_3,
-  //   BERAPAW_PLUGIN_4,
-  // ]);
-  // console.log("BeraPaw Bribe Rewards Distributed");
+  await voter.distributeToBribes([
+    BERAPAW_PLUGIN_0,
+    BERAPAW_PLUGIN_1,
+    BERAPAW_PLUGIN_2,
+    BERAPAW_PLUGIN_3,
+    BERAPAW_PLUGIN_4,
+  ]);
+  console.log("BeraPaw Bribe Rewards Distributed");
 
   // await voter.distributeToBribes([
   //   BURRBEAR_PLUGIN_0,
@@ -2247,30 +2247,30 @@ async function main() {
   // ]);
   // console.log("BurrBear Bribe Rewards Distributed");
 
-  // await voter.distributeToBribes([
-  //   TRIFECTA_PLUGIN_0,
-  //   TRIFECTA_PLUGIN_1,
-  //   TRIFECTA_PLUGIN_2,
-  // ]);
-  // console.log("Liquidity Trifecta Bribe Rewards Distributed");
+  await voter.distributeToBribes([
+    TRIFECTA_PLUGIN_0,
+    TRIFECTA_PLUGIN_1,
+    TRIFECTA_PLUGIN_2,
+  ]);
+  console.log("Liquidity Trifecta Bribe Rewards Distributed");
 
-  // await voter.distributeToBribes([
-  //   INFRARED_TRIFECTA_PLUGIN_0,
-  //   INFRARED_TRIFECTA_PLUGIN_1,
-  //   INFRARED_TRIFECTA_PLUGIN_2,
-  //   INFRARED_TRIFECTA_PLUGIN_3,
-  //   INFRARED_TRIFECTA_PLUGIN_4,
-  //   INFRARED_TRIFECTA_PLUGIN_5,
-  //   INFRARED_TRIFECTA_PLUGIN_6,
-  //   INFRARED_TRIFECTA_PLUGIN_7,
-  //   INFRARED_TRIFECTA_PLUGIN_8,
-  //   INFRARED_TRIFECTA_PLUGIN_9,
-  //   INFRARED_TRIFECTA_PLUGIN_10,
-  // ]);
-  // console.log("Infrared Trifecta Bribe Rewards Distributed");
+  await voter.distributeToBribes([
+    INFRARED_TRIFECTA_PLUGIN_0,
+    INFRARED_TRIFECTA_PLUGIN_1,
+    INFRARED_TRIFECTA_PLUGIN_2,
+    INFRARED_TRIFECTA_PLUGIN_3,
+    INFRARED_TRIFECTA_PLUGIN_4,
+    INFRARED_TRIFECTA_PLUGIN_5,
+    INFRARED_TRIFECTA_PLUGIN_6,
+    INFRARED_TRIFECTA_PLUGIN_7,
+    INFRARED_TRIFECTA_PLUGIN_8,
+    INFRARED_TRIFECTA_PLUGIN_9,
+    INFRARED_TRIFECTA_PLUGIN_10,
+  ]);
+  console.log("Infrared Trifecta Bribe Rewards Distributed");
 
-  // await voter.distributeToBribes([BULLAS_PLUGIN]);
-  // console.log("Game Bribe Rewards Distributed");
+  await voter.distributeToBribes([BULLAS_PLUGIN]);
+  console.log("Game Bribe Rewards Distributed");
 
   //===================================================================
   // 14. Remove Plugin
